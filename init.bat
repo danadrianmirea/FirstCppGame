1e18213f2000b318b69675243776d2e74f7d2d19
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-01 01:40:06 +0200

    add files

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# FirstCppGame" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:danadrianmirea/FirstCppGame.git
+git push -u origin main
